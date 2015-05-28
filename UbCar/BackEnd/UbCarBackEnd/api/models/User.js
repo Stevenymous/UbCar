f92956690e3962b375225bcb1b59f5518d339eff
steven <steven_martins@live.fr>
2015-05-28 11:49:34 +0200

    idem previous

--- UbCar/BackEnd/UbCarBackEnd/api/models/User.js
+++ UbCar/BackEnd/UbCarBackEnd/api/models/User.js
@@ -6,9 +6,57 @@
 */
 
 module.exports = {
-
+  identity: 'user',
+  connection: 'blablabla',
+  tableName: 'user',
   attributes: {
-
+  	//autoPK: true ?
+  	id: {
+      type: 'integer',
+      autoIncrement: true,
+      unique: true,
+      primaryKey: true,
+      columnName: 'id'
+    },
+    nom: {
+      type: 'string',
+      required: true,
+      size: 25,
+      columnName: 'nom'
+    },
+    prenom: {
+      type: 'string',
+      required: true,
+      size: 25,
+      columnName: 'prenom'
+    },
+    ville: {
+      type: 'string',
+      required: true,
+      size: 50,
+      columnName: 'ville'
+    },
+    place: {
+      type: 'integer',
+      required: true,
+      columnName: 'place'
+    },
+    mdp: {
+      type: 'string',
+      required: true,
+      columnName: 'mdp'
+    },
+    email: {
+      type: 'email',
+      required: true,
+      unique: true,
+      columnName: 'email'
+    }
+    // reference to trajet
+    trajets: {
+      collection: 'trajet',
+      via: 'owners',
+      dominant: true
+    }
   }
 };
-
